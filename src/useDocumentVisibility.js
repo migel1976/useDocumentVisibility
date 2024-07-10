4ca17b6c649d89d086c034e289a384fd17834640
migel1976 <migel1976@gmail.com>
2024-07-10 10:51:30 +0300

    устранил замечания

--- src/useDocumentVisibility.js
+++ src/useDocumentVisibility.js
@@ -1,16 +1,13 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 const useDocumentVisibility=()=>{
     const [count, setCount]=useState(0)
     const [visible, setVisible]=useState(true)
     const onVisible=()=>{
             if (document.visibilityState === "visible") {
-                console.log('страница активна')
                 setVisible(true)
             }
              else {
-                console.log('страница не активна')
                 setVisible(false)
                 setCount(x=>x+1)
             }
