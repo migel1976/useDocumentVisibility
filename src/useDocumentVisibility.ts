0564957575867876b29320d82d4b029460868d6d
migel1976 <migel1976@gmail.com>
2024-07-16 12:49:02 +0300

    устранил замечание на счет форматирование кода

--- src/useDocumentVisibility.ts
+++ src/useDocumentVisibility.ts
@@ -1,39 +1,37 @@
 import { useEffect, useState } from "react";
-type Callback=(visible: boolean)=>void;
+type Callback = (visible: boolean) => void;
 
-interface VisibilityReturn{
-    visible:boolean;
-    count:number;
-    onVisibilityChange:(callback: Callback)=>void;
+interface VisibilityReturn {
+    visible: boolean;
+    count: number;
+    onVisibilityChange: (callback: Callback) => void;
 }
 
-const useDocumentVisibility=():VisibilityReturn=>{
-    const [count, setCount]=useState(0)
-    const [visible, setVisible]=useState(true)
-    const onVisible=()=>{
+const useDocumentVisibility = () :VisibilityReturn => {
+    const [count, setCount] = useState(0);
+    const [visible, setVisible] = useState(true);
+    const onVisible = () => {
             if (document.visibilityState === "visible") {
-                console.log('страница активна')
-                setVisible(true)
+                setVisible(true);
             }
              else {
-                console.log('страница не активна')
-                setVisible(false)
-                setCount(x=>x+1)
+                setVisible(false);
+                setCount( x => x + 1);
             }
     }
 
-    const onVisibilityChange=(callback:Callback)=>{
-       return callback(visible)
+    const onVisibilityChange = (callback: Callback) => {
+       return callback(visible);
     }
 
-    useEffect(()=>{
-        document.addEventListener("visibilitychange", onVisible)
-        return ()=>document.removeEventListener("visibilitychange", onVisible)
-    },[])
+    useEffect(() => {
+        document.addEventListener("visibilitychange", onVisible);
+        return () => document.removeEventListener("visibilitychange", onVisible);
+    }, [])
     return {
         count, 
         visible,
         onVisibilityChange
     }
 }
-export default useDocumentVisibility
+export default useDocumentVisibility;
